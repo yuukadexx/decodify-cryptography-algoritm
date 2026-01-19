9bcb044015964eca50e5db9c7eae7844081cb1f0
kimzanst <mizanhakim06@gmail.com>
2026-01-19 07:39:52 -0800

    feat: update fitur mizan

--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\Python\crypto-web\venv'
+set -gx VIRTUAL_ENV 'C:\Project\decodify-cryptography-algoritm\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
